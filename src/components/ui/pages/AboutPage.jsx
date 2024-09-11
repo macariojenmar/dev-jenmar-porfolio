9a41e493d80c63ad60bb3285063eac6c84d164de
macariojenmar <macariojenmar05@gmail.com>
2024-09-11 21:25:30 +0800

    Refs to issue #6 and #5 created contact and about page

---
+++ src/components/ui/pages/AboutPage.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Box, Chip, Grid2, Typography } from "@mui/material";
+import { ResponsiveHeaderTypography } from "../../generic/GenericTypography";
+import AppLayout from "../../../layouts/AppLayout";
+import { ADOBE_XD_ICON, BOOTSTRAP_ICON, CSS_ICON, FIGMA_ICON, GIT_ICON, HTML_ICON, LARAVEL_ICON, MUI_ICON, MYSQL_ICON, REACT_JS_ICON } from "../../generic/GenericIcons";
+
+const technicalSkills = [
+  {
+    icon: REACT_JS_ICON,
+    label: 'React JS'
+  },
+  {
+    icon: MUI_ICON,
+    label: 'Material UI'
+  },
+  {
+    icon: LARAVEL_ICON,
+    label: 'Laravel'
+  },
+  {
+    icon: MYSQL_ICON,
+    label: 'MySQL'
+  },
+  {
+    icon: HTML_ICON,
+    label: 'HTML'
+  },
+  {
+    icon: CSS_ICON,
+    label: 'CSS'
+  },
+  {
+    icon: BOOTSTRAP_ICON,
+    label: 'Bootstrap'
+  },
+  {
+    icon: FIGMA_ICON,
+    label: 'Figma'
+  },
+  {
+    icon: ADOBE_XD_ICON,
+    label: 'Adobe Xd'
+  },
+  {
+    icon: GIT_ICON,
+    label: 'Git'
+  }
+];
+
+const AboutPage = () => {
+  return (
+    <AppLayout>
+      <Grid2 container alignItems={'center'} spacing={2}>
+        <Grid2 container item size={{ xs: 12, sm: 12, md: 6, lg: 6, xl: 6 }} justifyContent={{ xs: 'center', md: 'flex-start' }}>
+          <Box sx={{ width: { xs: '253px', md: '420px' }, height: { xs: '240px', md: '390px' } }}>
+            <img
+              src={'/aboutImage.png'}
+              alt="About Image"
+              style={{ width: '100%', height: '100%' }}
+            />
+          </Box>
+        </Grid2>
+        <Grid2 item size={{ xs: 12, sm: 12, md: 6, lg: 6, xl: 6 }}>
+          <ResponsiveHeaderTypography>ABOUT ME</ResponsiveHeaderTypography>
+          <Typography mb={3} mt={1} variant="body2">
+            I am Jenmar Sacyang Macario, a web developer based in Baguio City, Benguet, Philippines.
+            My tech stack primarily includes React JS, Material-UI (MUI), and Laravel.
+            I specialize in building single-page applications (SPA) and take pride in writing clean, maintainable code.
+            Collaboration is a key part of my workflow, as I believe that working together leads to better solutions and more efficient development processes.
+          </Typography>
+          <Typography fontWeight={700} mb={1}>Technical Skills</Typography>
+          {
+            technicalSkills?.map((item, index) => {
+              return (
+                <Chip
+                  key={`skills-key-${index}`}
+                  icon={item?.icon}
+                  label={<Typography variant="caption" fontWeight={700}>{item?.label}</Typography>}
+                  sx={{ margin: '0 6px 7px 0' }}
+                />
+              )
+            })
+          }
+        </Grid2>
+      </Grid2>
+    </AppLayout >
+  );
+};
+
+export default AboutPage;
---
+++ src/components/ui/pages/ContactPage.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { IconLabel, ResponsiveHeaderTypography } from "../../generic/GenericTypography";
+import AppLayout from "../../../layouts/AppLayout";
+import { Box, Grid2, Typography } from "@mui/material";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+
+const ContactPage = () => {
+  const contactList = [
+    {
+      label: 'Email',
+      icon: <MdOutlineEmail />,
+      value: 'macariojenmar05@gmail.com'
+    },
+    {
+      label: 'Address',
+      icon: <IoLocationOutline />,
+      value: 'Baguio City, Benguet, Philippines'
+    }
+  ];
+
+  return (
+    <AppLayout xl={'280px 410px'} lg={'190px 200px'}>
+      <Box padding={{ md: '0 240px' }}>
+        <ResponsiveHeaderTypography>CONTACT ME</ResponsiveHeaderTypography>
+        <Typography variant="body2" mb={3} mt={1}>
+          If you are considering hiring me, feel free to reach out via the email provided below.
+          Curious about what I bring to the table? Take a look at my projects or resume 😉
+        </Typography>
+        <Grid2 container justifyContent={'space-between'}>
+          {
+            contactList?.map((item, index) => {
+              return (
+                <Grid2 item key={`contact-key-${index}`}>
+                  <IconLabel
+                    icon={item?.icon}
+                    label={<Typography variant="body2">{item?.label}</Typography>}
+                    color={'grey'}
+                  />
+                  <Typography fontWeight={700} mb={2}>{item?.value}</Typography>
+                </Grid2>
+              );
+            })
+          }
+        </Grid2>
+      </Box>
+    </AppLayout>
+  );
+};
+
+export default ContactPage;
